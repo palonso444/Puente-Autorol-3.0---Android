146b0197858b5427d17eb9e7ff951bae1f8b049b
palonso444 <73613508+palonso444@users.noreply.github.com>
2024-02-20 21:02:23 +0100

    Update autorol_utils.py

--- autorol_utils.py
+++ autorol_utils.py
@@ -1,17 +1,54 @@
-######### FUNCTIONS TO CONVERT JSON FILE FROM AUTOROL 3.0 INTO AN APP#########
 
+
+##################################################################################################################
+##                    FUNCTIONS TO EXTRACT RELEVANT INFO FROM JSON FILE FROM AUTOROL 3.0 INTO AN APP            ##
+##################################################################################################################
 
 import json
+import re
 
 
 
-def get_scenes(path):
+def read_json(path):
 
-    with open(path) as file:
+    with open(path, 'r') as file:       #open json in read ('r') mode.
         
         jsonfile = json.load(file)
 
-        return jsonfile ['escenas']
+    return jsonfile
+
+
+
+
+def get_scenes(jsonfile, format = False):
+
+    if format:
+
+        format_kivy_all (jsonfile)
+        
+    return jsonfile ['escenas']
+
+            
+
+
+def format_kivy_all (jsonfile):
+
+    for scene in jsonfile['escenas']:
+
+                for text in scene['textos']:
+
+                    if text['texto'][:2] != '<p': #some text does not have new paragraph tag. Must be added
+
+                        text['texto'] = '<p>'+ text['texto'] + '</p>'  
+
+                    text['texto'] = format_kivy (text['texto'])
+
+                    for link in text['enlaces']:
+
+                        link['texto'] = re.sub(r'<[^>]*>', '', link['texto'])
+
+        
+    return jsonfile
 
 
 
@@ -25,7 +62,8 @@ def get_scene(id, scenes):
         
 
 
-def get_intro (scenes):
+
+def get_intro (scenes, id_only = False):
 
     links = get_all_links(scenes)
 
@@ -33,12 +71,18 @@ def get_intro (scenes):
 
         if scene['id'] not in links:
 
-            return scene
+            if id_only:
+
+                return scene['id']
+            
+            else:
+            
+                return scene
 
 
         
-
 ################################ VARIABLES ###############################
+
 
 
 def get_variables(scenes):
@@ -51,7 +95,7 @@ def get_variables(scenes):
         
             for condition in text['condiciones']:
 
-                variables.update({condition['item']: 0})
+                variables.update({condition['variable']: 0})
 
         for text in scene ['textos']:
         
@@ -59,24 +103,46 @@ def get_variables(scenes):
 
                 for condition in link ['condiciones']:
 
-                    variables.update({condition['item']: 0})
+                    variables.update({condition['variable']: 0})
 
     return variables
 
 
 
-def variables_update (variables, text):
+def compare_conditions(variables, conditions):
 
-    consequences = get_text_consequences(text)
+    for key in variables:
 
-    variables.update(consequences)
+        if key in conditions and conditions[key] != variables[key]:
 
-    return variables
+            return False
+
+    return True 
 
 
 
+def get_conditions(item):
+
+    item_conditions = dict()
+
+    for condition in item['condiciones']:
+
+        item_conditions.update({condition['variable']: int(condition['valorComparar'])})
+
+    return item_conditions
 
 
+
+def get_consequences(item):
+
+    
+    item_consequences = dict()
+
+    for consequence in item['consecuencias']:
+
+        item_consequences.update({consequence['variable']: int(consequence['valor'])})
+
+    return item_consequences
 
 
 
@@ -85,51 +151,121 @@ def variables_update (variables, text):
 ######################################### TEXT ################################################
 
 
-def get_text(scene, object = True):
+
+def get_text(scene):
     
     texts = list ()
     
-    if object:
-    
-        for text in scene ['textos']:
+    for text in scene ['textos']:
 
-            texts.append(text)
-
-    elif not object:
-
-        for i in range (len(scene ['textos'])):
-
-            texts.append (scene['textos'][i]['texto'])
+        texts.append(text)
 
     return texts
 
 
-def get_text_conditions (text):
-
-    text_conditions = dict()
-
-    for condition in text['condiciones']:
-
-        text_conditions.update({condition['item']: int(condition['valorComparar'])})
-
-    return text_conditions
 
 
+def format_kivy(text):               #default 0, buttons do not pass index so /n/n are not removed
+    
 
-def get_text_consequences (text):
+    #CHECK FOR IMAGE
 
     
-    text_consequences = dict()
+    if text.find('<img src=') != -1:    #if text has a image tag (text.find returns -1 if not found)
+        
+        clean_text = get_image(text)      #get_image() will delete any text not part of the tag
 
-    for consequence in text['consecuencias']:
+        return clean_text
 
-        text_consequences.update({consequence['item']: int(consequence['valor'])})
 
-    return text_consequences
+    
+    #TEXT ALIGNMENT
+
+    text = re.sub(r'<p\s+style="text-align:\s*center;\s*">', r'[$center]', text, count = 1)     #if not first text, no newline character
+
+
+    
+    #NEW LINE CHARACTERS
+    
+    text = re.sub(r'<p.*?>', r'\n', text)         #r is for raw string. Treats \ as regular characters and not as escape characters
+    
+    text = re.sub(r'</p.*?>', r'\n', text)
+    
+    
+
+    #REMOVING USELESS TAGS
+
+    for tag in ['span', 'div', 'br']:             #tags to delete
+    
+        text = re.sub(r'<'+ tag +'.*?>', '', text)
+
+        text = re.sub(r'</'+ tag +'.*?>', '', text)
+
+    
+
+    #FORMATTING USEFUL TAGS
+
+    for tag in ['i', 'u', 'b']:                   #tags to kivy format
+    
+        text = re.sub(r'<'+ tag +'.*?>', r'['+ tag +']', text)
+
+        text = re.sub(r'</'+ tag +'.*?>', r'[/'+ tag +']', text)
+
+        text = re.sub(r'\[' + tag + r'\]\[/' + tag + r'\]', '', text) #this monster removes any tag [x] [/x] with no string in between
+                                                                                            
+
+
+    # FINAL TOUCHES
+
+    text = re.sub(r'\n\s*\n', r'\n\n', text)             #replaces any occurrence of more than 2 \n in a row by just 2 \n
+
+    text = re.sub(r'\n\n+$', r'\n', text)                #texts must finish only with one \n. Removes extra \n, if any, at the end of text
+
+    text = re.sub(r'\[/i\]\n\n\[i\]', r'[/i]\n[i]', text)   #removes one \n in case of interlines between italics verses (tipically poems or songs)
+             
+    text = re.sub(r'^\n\s*', '', text)                      #removes any \n at the start of text and texts consisting only of \n
+      
+    clean_text = re.sub(r'&nbsp;', ' ', text)           #removes residual 'non-breaking space' characters
+
+    
+    return clean_text
+
+
+
+def get_image(text):
+
+    pattern = r'/([^/]+\.(?:png|jpeg|jpg))'     #images as .png or .jpeg are supported
+    
+    match = re.search(pattern, text)
+        
+    if match:
+    
+        image_name = match.group(1)
+
+        return '[$image]' + image_name
+
+
+
+def align (text):
+
+    
+    if text [:9] == '[$center]':
+
+        alignment = 'center'
+
+    else:
+
+        alignment = 'left'
+    
+
+    text = re.sub(r'\[\$center\]', '', text)       #removes any [$center] tag that accidentally is the text
+        
+    return [text, alignment]
 
 
 
 ###################################################### LINKS #########################################
+
 
 
 def get_all_links (scenes):
@@ -147,7 +283,22 @@ def get_all_links (scenes):
     return links
 
 
-def get_links (text):
+
+def get_links(text):
+
+    links = list ()
+        
+    for link in text ['enlaces']:
+
+        links.append(link)
+
+    
+    return links
+
+
+
+
+def get_links_fates (text):
 
     links = dict()
     
@@ -155,4 +306,4 @@ def get_links (text):
 
         links.update({link['texto']: link['destinoExito']})
 
-    return links
+    return links    
